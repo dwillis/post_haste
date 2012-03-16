65b9ba33b03b13b266d00381e26ce055317db8dd
Derek Willis <dwillis@gmail.com>
2012-03-16 08:59:48 -0400

    added test for blog posts

--- test/post_haste/test_article.rb
+++ test/post_haste/test_article.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class TestPostHaste::TestArticle < Test::Unit::TestCase
 	include PostHaste
 	
-	context "Article.create" do
+	context "Article.create from article" do
 		setup do
 		  url = "http://www.washingtonpost.com/politics/joe-biden-digging-back-into-his-roots-to-move-obama-forward/2012/03/14/gIQARwYBDS_story.html"
 		  json_url = Article.get_json(url)
@@ -13,6 +13,7 @@ class TestPostHaste::TestArticle < Test::Unit::TestCase
 		
 		should "return an object of the Article type" do
 			assert_kind_of(Article, @article)
+			assert_equal(@article.type, 'article')
 		end
 		
 		%w(uuid type title blurb).each do |attr|
@@ -20,6 +21,25 @@ class TestPostHaste::TestArticle < Test::Unit::TestCase
 				assert_equal(@result['contentConfig'][attr], @article.send(attr))
 			end
 		end
+  end
+
+  context "Article.create from blog post" do
+		setup do
+		  url = "http://www.washingtonpost.com/blogs/the-fix/post/republicans-on-the-2012-gop-field-blah/2012/03/15/gIQAT7CSFS_blog.html"
+		  json_url = Article.get_json(url)
+		  @result = Article.parse_json(json_url)
+			@article = Article.create(@result)
+		end
 		
+		should "return an object of the Article type" do
+			assert_kind_of(Article, @article)
+			assert_equal(@article.type, 'BlogStory')
+		end
+		
+		%w(uuid type title blurb).each do |attr|
+			should "assign the value of the @#{attr} attribute from the '#{attr}' key in the hash" do
+				assert_equal(@result['contentConfig'][attr], @article.send(attr))
+			end
+		end
   end
 end

1f527f9cd9374371e83fc0bbae5b6f895faa369e
Derek Willis <dwillis@gmail.com>
2013-11-12 11:38:02 -0500

    added permalinks for comments

--- lib/post_haste/comment.rb
+++ lib/post_haste/comment.rb
@@ -6,7 +6,7 @@ module PostHaste
   class Comment
     # Represents a comment on a Washington Post story
     
-    attr_reader :id, :article_url, :author, :content, :status, :published 
+    attr_reader :id, :article_url, :author, :content, :status, :published , :permalink
     
     def initialize(params={})
       params.each_pair do |k,v|
@@ -17,10 +17,14 @@ module PostHaste
     def self.create_comments_from_objects(article_url, comments)
       results = []
       comments.each do |comment|
-        c = Comment.new({:id => comment['object']['id'], :article_url => article_url, :author => comment['actor']['title'], :content => comment['object']['content'], :status => comment['object']['status'], :published => DateTime.parse(comment['object']['published'])})
+        c = Comment.new({:id => comment['object']['id'].gsub('http://',''), :article_url => article_url, :author => comment['actor']['title'], :content => comment['object']['content'], :status => comment['object']['status'], :published => DateTime.parse(comment['object']['published'])})
         results << c
       end
       results
+    end
+    
+    def permalink
+      article_url+'?commentID='+id
     end
     
   end

956210fe60ce2f8b6acc75fef6384516601b3240
Derek Willis <dwillis@gmail.com>
2015-09-16 17:04:45 -0400

    added content type and rank

--- lib/post_haste/most_viewed.rb
+++ lib/post_haste/most_viewed.rb
@@ -1,7 +1,7 @@
 module PostHaste
   class MostViewed
 
-    attr_reader :platform, :datetime, :title, :byline, :url
+    attr_reader :platform, :datetime, :title, :byline, :url, :rank, :type
 
     def initialize(params={})
       params.each_pair do |k,v|
@@ -11,26 +11,28 @@ module PostHaste
 
     def self.all(platform='web', limit=25)
       url = "http://js.washingtonpost.com/most-viewed/#{platform}/article,blog.feed-#{limit}.json"
-      create(HTTParty.get(url).parsed_response, platform)
+      create(HTTParty.get(url).parsed_response, platform, 'all')
     end
 
     def self.articles(platform='web', limit=25)
       url = "http://js.washingtonpost.com/most-viewed/#{platform}/article.feed-#{limit}.json"
-      create(HTTParty.get(url).parsed_response, platform)
+      create(HTTParty.get(url).parsed_response, platform, 'articles')
     end
 
     def self.blogs(platform='web', limit=25)
       url = "http://js.washingtonpost.com/most-viewed/#{platform}/blog.feed-#{limit}.json"
-      create(HTTParty.get(url).parsed_response, platform)
+      create(HTTParty.get(url).parsed_response, platform, 'blogs')
     end
 
-    def self.create(result={}, platform)
-      result['content'].map{|r|
+    def self.create(result={}, platform, type)
+      result['content'].map!.with_index {|r,i|
         self.new platform: platform,
+                 type: type,
                  datetime: DateTime.parse(result['updated']),
                  title: r['linkText'],
                  byline: r['byline'],
-                 url: r['url']
+                 url: r['url'],
+                 rank: i+1
         }
     end
   end
